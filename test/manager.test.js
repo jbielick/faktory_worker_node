1932c8971269018dd18db425abf0961140a74a65
Josh Bielick <jbielick@gmail.com>
2017-11-02 20:31:59 -0400

    create manager->processor pool
    major point of this is to create a pool of processors and a connection
    pool. The connection pool holds concurrency + 2 connections to the
    faktory server and each processor has it's own work loop and fetch
    cycle. The connection pool is managed by the manager and all lifecycle
    hooks are syndicated to the pool of workers. A processor is an object
    that polls for jobs--checking out a connection for each command it
    executes. It's working loop is an infinite for loop with awaiting on the
    fetch and job execution.
    
                      ,_______________,
                      |    faktory    |
                      -----------------
                              |
                      ,________________
                     /        |        \
         ,___________,  ,___________,  ,___________,
         | processor |  | processor |  | processor |
         -------------  -------------  -------------
                      \       |       /
                       ---------------
                              |
                       ,-------------,
                       |   manager   |
                       ---------------
    
    Where each processor has its own connection to the faktory server and
    processor pools are managed by a manager. A manager has n concurrency
    and that's the number of processors and connections that will be started
    to serve that pool. All of this lives within one node process.
    
    Benchmarks:
    
    pool:
    
    › bench/push
    
    pushed: 30000 duration: 1.35946858s jobs/s: 22067
    
    › bench/push
    
    pushed: 30000 duration: 1.430357713s jobs/s: 20974
    
    › bench/push
    
    pushed: 30000 duration: 1.3823066609999999s jobs/s: 21703
    
    › bench/work
    
    Jobs processed: 30000
    Concurrency: 20
    
    Duration: 6.7563684649999995s
    Jobs/s: 4440
    
    › bench/work
    
    Jobs processed: 30000
    Concurrency: 20
    
    Duration: 6.653886883s
    Jobs/s: 4509
    
    › bench/work
    
    Jobs processed: 30000
    Concurrency: 20
    
    Duration: 6.515825339s
    Jobs/s: 4604
    
    ===============================================================
    
    pool without await this.execute and sleeping in the loop
    
    Jobs processed: 30000
    Concurrency: NaN
    
    Duration: 5.220679224s
    Jobs/s: 5746
    
    ================================================================
    
    pool without log
    
    › bench/work
    
    Jobs processed: 30000
    Concurrency: 20
    
    Duration: 3.288809578s
    Jobs/s: 9122
    
    › bench/work
    
    Jobs processed: 30000
    Concurrency: 20
    
    Duration: 3.31144746s
    Jobs/s: 9059
    
    › bench/work
    
    Jobs processed: 30000
    Concurrency: 20
    
    Duration: 3.2611014s
    Jobs/s: 9199
    
    ================================================================
    
    master:
    
    › bench/push
    
    pushed: 30000 duration: 1.382443728s jobs/s: 21701
    
    › bench/push
    
    pushed: 30000 duration: 1.378380277s jobs/s: 21765
    
    › bench/push
    
    pushed: 30000 duration: 1.376629829s jobs/s: 21792
    
    › bench/work
    
    Jobs processed: 30000
    Concurrency: 20
    
    Duration: 4.639131743s
    Jobs/s: 6467
    
    › bench/push
    
    Jobs processed: 30000
    Concurrency: NaN
    
    Duration: 4.458317899s
    Jobs/s: 6729
    
    › bench/push
    
    Jobs processed: 30000
    Concurrency: NaN
    
    Duration: 4.361462364s
    Jobs/s: 6878
    
    ================================================
    
    master w/ connection pool
    
    › bench/work
    
    Jobs processed: 30000
    Concurrency: NaN
    
    Duration: 4.595406734s
    Jobs/s: 6528
    
    › bench/work
    
    Jobs processed: 30000
    Concurrency: NaN
    
    Duration: 4.602304733s
    Jobs/s: 6518

---
+++ test/manager.test.js
@@ -0,0 +1,93 @@
+require('./helper');
+const test = require('ava');
+const {
+  createJob,
+  createClient,
+  queueName,
+  withConnection: connect
+} = require('faktory-client/test/support/helper');
+
+const Manager = require('../lib/manager');
+
+test('creates processor pool size of concurrency', t => {
+  const concurrency = 10;
+  const manager = create({ concurrency });
+  t.is(manager.processors.length, concurrency, 'correct number of processors');
+});
+
+test('.quiet sends processors QUIET', async t => {
+  const manager = create();
+
+  manager.quiet();
+
+  t.truthy(manager.processors.length, 'no processors to check');
+  manager.processors.forEach((p) => {
+    t.truthy(p._quiet, 'processor did not receive QUIET');
+  })
+});
+
+test('.busy reports jobs in progress', async t => {
+  const { queue, jobtype } = await push();
+
+  await new Promise((resolve) => {
+    const manager = create({
+      concurrency: 1,
+      queues: [queue],
+      registry: {
+        [jobtype]: async () => {
+          t.is(manager.busy.length, 1, '.busy did not report job in progress');
+          manager.stop();
+          resolve();
+        }
+      }
+    });
+
+    manager.run();
+  });
+});
+
+test('.stop allows in-progress jobs to finish', async t => {
+  const { queue, jobtype } = await push();
+
+  await new Promise((resolve) => {
+    const manager = create({
+      concurrency: 1,
+      queues: [queue],
+      shutdownTimeout: 100,
+      registry: {
+        [jobtype]: async () => {
+          await sleep(50);
+          t.pass();
+          resolve();
+        }
+      }
+    });
+
+    manager.run();
+    setTimeout(() => {
+      manager.stop();
+    }, 10);
+  });
+});
+
+function create(...args) {
+  return new Manager(...args);
+}
+
+async function push(opts = {}) {
+  const queue = opts.queue || queueName();
+  const jobtype = 'TestJob';
+  const args = opts.args || [];
+  await connect(async (client) => {
+    await client.push({
+      jobtype,
+      queue,
+      args
+    });
+  });
+  return { queue, jobtype, args };
+};
+
+function sleep(ms, value = true) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
