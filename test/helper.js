02d6f07a4205c833db90e0314354eb0fe6921dba
Josh Bielick <jbielick@gmail.com>
2018-03-30 12:20:34 -0400

    upgrade faktory-client 0.6.1
    job defaults are not set on the server, thus, they were added to
    faktory-client.
    
    closes #7

--- test/helper.js
+++ test/helper.js
@@ -1,41 +1,13 @@
-const net = require('net');
 const debug = require('debug')('faktory-worker:test-helper');
 const {
   queueName,
-  withConnection
+  withConnection,
+  mockedServer,
+  mocked
 } = require('faktory-client/test/support/helper');
 
 const sleep = (ms, value = true) => {
   return new Promise(resolve => setTimeout(() => resolve(value), ms));
-};
-
-const mockServer = () => {
-  const server = net.createServer();
-  let shookhands = false;
-  server.on('connection', (socket) => {
-    socket.write("+HI {\"v\":2}\r\n");
-
-    server.once('HELLO', (msg, socket) => {
-      socket.write("+OK\r\n");
-    });
-
-    socket.on('data', async (chunk) => {
-      const msg = chunk.toString();
-      server.emit(msg.split(' ')[0], msg, socket);
-    });
-  });
-  return server;
-}
-
-const mocked = async (fn) => {
-  const server = mockServer();
-  const port = Math.round(Math.random() * 100 + 7000, 0);
-  server.listen(port, '127.0.0.1');
-  try {
-    return fn(server, port);
-  } finally {
-    await new Promise((resolve) => server.close(() => resolve()));
-  }
 };
 
 const push = async (opts = {}) => {
@@ -55,6 +27,6 @@ const push = async (opts = {}) => {
 module.exports = {
   sleep,
   push,
-  mockServer,
+  mockedServer,
   mocked
 };
