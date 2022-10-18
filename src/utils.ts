2bab0377acffeee9973db76fe46559df8f4b8ac5
Steve Pletcher <4468322+ThePletch@users.noreply.github.com>
2022-10-18 11:17:53 -0400

    please accept my sacrifice, codeclimate gods

--- src/utils.ts
+++ src/utils.ts
@@ -34,6 +34,23 @@ incorrect:\treject('message')
 }
 
 /**
+ * internal function to generate a hashed string. the exported version below wraps it in a cache.
+ */
+function generateHash(
+  password: string,
+  salt: string,
+  iterations: number
+): string {
+  let hash = createHash("sha256").update(`${password}${salt}`);
+
+  for (let i = 1; i < iterations; i += 1) {
+    hash = createHash("sha256").update(hash.digest());
+  }
+
+  return hash.digest("hex");
+}
+
+/**
  * hashes the password with server-provided salt
  * @param  {String}  password            the password to the faktory server
  * @param  {String}  salt                the server-provided salt to use in hashing
@@ -45,20 +62,6 @@ incorrect:\treject('message')
  */
 export const hash = (function () {
   const hashCache: Record<string, Record<string, Record<number, string>>> = {};
-
-  function generateHash(
-    password: string,
-    salt: string,
-    iterations: number
-  ): string {
-    let hash = createHash("sha256").update(`${password}${salt}`);
-
-    for (let i = 1; i < iterations; i += 1) {
-      hash = createHash("sha256").update(hash.digest());
-    }
-
-    return hash.digest("hex");
-  }
 
   return function hash(
     password: string,
